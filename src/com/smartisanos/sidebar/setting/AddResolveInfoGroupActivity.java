47c1667e6c6b140e1dbfe4832d3005eebd123c48
XiNGRZ <chenxingyu92@gmail.com>
2017-01-03 22:50:30 +0800

    OneStep: Materialize (Stage 4: Add app Activity)
    Change-Id: Ib6d76328a5c4c83d5df3ebc5601297d53227c42c

--- src/com/smartisanos/sidebar/setting/AddResolveInfoGroupActivity.java
+++ src/com/smartisanos/sidebar/setting/AddResolveInfoGroupActivity.java
@@ -2,9 +2,6 @@ package com.smartisanos.sidebar.setting;
 
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
@@ -13,7 +10,7 @@ import com.smartisanos.sidebar.SidebarController;
 import com.smartisanos.sidebar.SidebarStatus;
 import com.smartisanos.sidebar.util.Utils;
 
-public class AddResolveInfoGroupActivity extends AppCompatActivity {
+public class AddResolveInfoGroupActivity extends BaseActivity {
 
     private static final int MAX_TIMES = 5;
     private static final String KEY_TIMES = "hint_sort_by_long_press_times";
@@ -21,14 +18,13 @@ public class AddResolveInfoGroupActivity extends AppCompatActivity {
     private View mContainer;
     private AddResolveInfoGroupAdapter mAdapter;
 
+    @Override
+    protected int provideContentView() {
+        return R.layout.add_resolve_layout;
+    }
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_resolve_layout);
-
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-
-        //noinspection ConstantConditions
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mContainer = findViewById(R.id.container);
 
@@ -38,17 +34,6 @@ public class AddResolveInfoGroupActivity extends AppCompatActivity {
         mListView.setAdapter(mAdapter);
 
         showHintToast();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                supportFinishAfterTransition();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
     private void showHintToast() {
